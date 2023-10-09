06b16ddc7c4db7e95690d86f450e846f5b483005
Daisy Hong <hdaisywd@gmail.com>
2023-10-10 01:44:17 +0900

    Feat: Project 새로 생성

---
+++ CodeFantasia/MainVC.swift
@@ -0,0 +1,11 @@
+
+import Foundation
+import UIKit
+
+class MainVC: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+        
+    }
+}
