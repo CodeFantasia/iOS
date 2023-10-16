4baa6785c21986ad1307c2dbfd3b00fe5ddda671
“hw3590” <lkhs44@naver.com>
2023-10-16 20:00:28 +0900

    Design. "일부 뷰 재수정 및 구분선 추가"

--- CodeFantasia/ProfileViewController.swift
+++ CodeFantasia/ProfileViewController.swift
@@ -4,7 +4,6 @@
 //
 //  Created by Hyunwoo Lee on 2023/10/12.
 //
-
 import UIKit
 import SnapKit
 
@@ -23,58 +22,61 @@ class ProfileViewController: UIViewController {
         label.textColor = .black
         return label
     }()
-    
+
     private let produceView: UIView = {
         let view = UIView()
-        view.layer.cornerRadius = 10
-        view.layer.shadowOpacity = 0.5
-        view.layer.shadowRadius = 10
+        view.layer.cornerRadius = 8
+        view.layer.shadowOpacity = 0.2
+        view.layer.shadowRadius = 8
         view.layer.shadowOffset = CGSize(width: 0.5, height: 0.5)
         view.layer.shadowColor = UIColor.black.cgColor
         view.backgroundColor = .white
         return view
     }()
-    
+
     private let profileImage: UIImageView = {
         let image = UIImageView()
         image.layer.cornerRadius = 50
         image.backgroundColor = .gray
         return image
     }()
-    
+
     private let nicknameLabel: UILabel = {
         let label = UILabel()
         label.text = "닉네임"
         label.font = UIFont.systemFont(ofSize: 20, weight: .semibold)
         return label
     }()
-    
+
     private let produceLabel: UILabel = {
         let label = UILabel()
         label.text = "내 소개"
         label.font = UIFont.systemFont(ofSize: 20, weight: .semibold)
         return label
     }()
-    
+
     private let produceContent: UILabel = {
         let label = UILabel()
         label.numberOfLines = 3
         label.font = UIFont.systemFont(ofSize: 16, weight: .regular)
         return label
     }()
-    
+
     private lazy var stackView: UIStackView = {
         let stackView = UIStackView(arrangedSubviews: [
             titleLabel,
             produceView,
             infoLabel,
-            separatorView,
+            separatorView1,
             techTitleLabel,
             techLabel,
+            separatorView2,
             urlTitleLabel,
             urlLabel,
+            separatorView3,
             interestTitleLabel,
             interestLabel,
+            separatorView4,
             editButton,
             logoutButton
         ])
@@ -83,15 +85,30 @@ class ProfileViewController: UIViewController {
         stackView.spacing = 16
         return stackView
     }()
-    
+
     private let infoLabel: UILabel = {
         let label = UILabel()
-        label.text = "내 정보"
+        label.text = "나의 스펙 정보"
         label.font = UIFont.systemFont(ofSize: 20, weight: .semibold)
         return label
     }()
-    
-    private let separatorView: UIView = {
+
+    private let separatorView1: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.black
+        return view
+    }()
+    private let separatorView2: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.systemGray
+        return view
+    }()
+    private let separatorView3: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.systemGray
+        return view
+    }()
+    private let separatorView4: UIView = {
         let view = UIView()
         view.backgroundColor = UIColor.black
         return view
@@ -103,58 +120,58 @@ class ProfileViewController: UIViewController {
         label.font = UIFont.systemFont(ofSize: 16, weight: .semibold)
         return label
     }()
-    
+
     private let techLabel: UILabel = {
         let label = UILabel()
         label.text = "Swift"
         label.font = UIFont.systemFont(ofSize: 14, weight: .regular)
         return label
     }()
-    
+
     private let urlTitleLabel: UILabel = {
         let label = UILabel()
         label.text = "나의 포트폴리오"
         label.font = UIFont.systemFont(ofSize: 16, weight: .semibold)
         return label
     }()
-    
+
     private let urlLabel: UILabel = {
         let label = UILabel()
         label.text = "www.github.com"
         label.font = UIFont.systemFont(ofSize: 14, weight: .regular)
         return label
     }()
-    
+
     private let interestTitleLabel: UILabel = {
         let label = UILabel()
         label.text = "나의 관심 분야"
         label.font = UIFont.systemFont(ofSize: 16, weight: .semibold)
         return label
     }()
-    
+
     private let interestLabel: UILabel = {
         let label = UILabel()
         label.text = "앱개발"
         label.font = UIFont.systemFont(ofSize: 14, weight: .regular)
         return label
     }()
-    
+
     private let editButton: UIButton = {
         let button = UIButton()
         button.backgroundColor = .blue
         button.setTitle("프로필 수정", for: .normal)
-        button.layer.cornerRadius = 10
+        button.layer.cornerRadius = 8
         return button
     }()
-    
+
     private let logoutButton:  UIButton = {
         let button = UIButton()
         button.backgroundColor = .gray
         button.setTitle("로그아웃", for: .normal)
-        button.layer.cornerRadius = 10
+        button.layer.cornerRadius = 8
         return button
     }()
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         setupLayout()
@@ -162,14 +179,14 @@ class ProfileViewController: UIViewController {
 }
 extension ProfileViewController {
     private func setupLayout() {
-        
+
         view.addSubview(scrollView)
         scrollView.addSubview(stackView)
         produceView.addSubview(profileImage)
         produceView.addSubview(nicknameLabel)
         produceView.addSubview(produceLabel)
         produceView.addSubview(produceContent)
-        
+
         scrollView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
@@ -183,15 +200,16 @@ extension ProfileViewController {
             $0.trailing.equalTo(scrollView.snp.trailing)
             $0.top.equalTo(scrollView)
             $0.width.equalTo(view)
-            $0.bottom.equalTo(scrollView)
+            $0.bottom.equalTo(scrollView.snp.bottom)
         }
-        
+
         titleLabel.snp.makeConstraints {
             $0.top.equalTo(stackView.snp.top).offset(10)
             $0.leading.equalTo(stackView.snp.leading).offset(20)
         }
         produceView.snp.makeConstraints {
-            $0.leading.trailing.equalTo(stackView).inset(20)
+            $0.leading.equalTo(stackView.snp.leading).inset(20)
+            $0.trailing.equalTo(stackView.snp.trailing).inset(20)
             $0.height.equalTo(250)
         }
         profileImage.snp.makeConstraints {
@@ -211,16 +229,29 @@ extension ProfileViewController {
             $0.top.equalTo(produceLabel.snp.bottom).offset(16)
             $0.leading.equalTo(produceLabel)
         }
-        separatorView.snp.makeConstraints {
+        separatorView1.snp.makeConstraints {
             $0.height.equalTo(1)
-            $0.leading.trailing.equalTo(stackView).inset(20)
+            $0.leading.equalTo(stackView.snp.leading).inset(20)
+            $0.trailing.equalTo(stackView.snp.trailing).inset(20)
+        }
+        separatorView2 .snp.makeConstraints {
+            $0.height.equalTo(1)
+            $0.leading.trailing.equalTo(separatorView1)
+        }
+        separatorView3.snp.makeConstraints {
+            $0.height.equalTo(1)
+            $0.leading.trailing.equalTo(separatorView1)
+        }
+        separatorView4.snp.makeConstraints {
+            $0.height.equalTo(1)
+            $0.leading.trailing.equalTo(separatorView1)
         }
         editButton.snp.makeConstraints {
-            $0.leading.trailing.equalTo(stackView).inset(20)
+            $0.leading.trailing.equalTo(separatorView1)
         }
         logoutButton.snp.makeConstraints {
-            $0.leading.trailing.equalTo(stackView).inset(20)
-            $0.bottom.equalTo(stackView.snp.bottom)
+            $0.leading.trailing.equalTo(separatorView1)
+            $0.bottom.equalTo(scrollView.snp.bottom).inset(20)
         }
     }
 }
