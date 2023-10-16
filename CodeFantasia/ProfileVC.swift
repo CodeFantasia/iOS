8dcc668cf63ed14cf1a3ea37d3d79a1c6186f637
YoungDuck1592 <tjdudejr1919@gmail.com>
2023-10-16 21:55:31 +0900

    Feat: "프로젝트 게시판 완성"
    프로젝트 게시판 UI 완성, 탭바 Clear

--- CodeFantasia/ProfileVC.swift
+++ CodeFantasia/ProfileVC.swift
@@ -3,6 +3,21 @@
 //  CodeFantasia
 //
 //  Created by 서영덕 on 10/13/23.
-//
+//  탭바를 위해 임시로 만든 파일 추후에 프로필 페이지 완성 시 삭제 후 탭바 수정 필요
 
-import Foundation
+import UIKit
+import SnapKit
+
+
+class ProfileVC: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let label = UILabel()
+        label.text = "세 번째 탭"
+        view.addSubview(label)
+        
+        label.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
+    }
+}
--- CodeFantasia/ProjectBoardVC.swift
+++ CodeFantasia/ProjectBoardVC.swift
@@ -1,8 +1,115 @@
 //
-//  ProjectBoard.swift
+//  ProjectBoardVC.swift
 //  CodeFantasia
 //
 //  Created by 서영덕 on 10/13/23.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+
+class ProjectBoardVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.register(ProjectBoardTableviewCell.self, forCellReuseIdentifier: "ProjectBoardCell")
+        return tableView
+    }()
+    
+    let mockData: [(image: UIImage?, title: String, detail: String, icons: [IconModel], status: String)] = [
+        (UIImage(named: "digimon"), "Project A", "This is the detail for Project A,This is the detail for Project A, This is the detail for Project A,This is the detail for Project A, This is the detail for Project A,This is the detail for Project A", [IconModel(image: UIImage(named: "swift") ?? UIImage()), IconModel(image: UIImage(named: "javascript") ?? UIImage())], "모집 중"),
+        (UIImage(named: "pokemon"), "Project B", "This is the detail for Project B", [IconModel(image: UIImage(named: "python") ?? UIImage())], "모집 완료"),
+    ]
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        setupNavigationBar()
+        setupTableView()
+        
+        view.backgroundColor = .white
+        navigationController?.navigationBar.backgroundColor = .white
+        navigationController?.navigationBar.tintColor = .black
+    }
+    
+    func setupNavigationBar() {
+        let customTitleLabel = UILabel()
+        customTitleLabel.text = "프로젝트 게시판"
+        customTitleLabel.font = UIFont.boldSystemFont(ofSize: 21)
+
+        let customTitleBarItem = UIBarButtonItem(customView: customTitleLabel)
+        navigationItem.leftBarButtonItem = customTitleBarItem
+        
+        let searchButtonView = UIButton(type: .system)
+        searchButtonView.setImage(UIImage(systemName: "magnifyingglass"), for: .normal)
+        searchButtonView.addTarget(self, action: #selector(searchButtonTapped), for: .touchUpInside)
+
+        let bellButtonView = UIButton(type: .system)
+        bellButtonView.setImage(UIImage(systemName: "bell"), for: .normal)
+        bellButtonView.addTarget(self, action: #selector(bellButtonTapped), for: .touchUpInside)
+
+        let pencilButtonView = UIButton(type: .system)
+        pencilButtonView.setImage(UIImage(systemName: "pencil"), for: .normal)
+        pencilButtonView.addTarget(self, action: #selector(pencilButtonTapped), for: .touchUpInside)
+
+        let stackView = UIStackView(arrangedSubviews: [searchButtonView, bellButtonView, pencilButtonView])
+        stackView.axis = .horizontal
+        stackView.spacing = 10
+
+        let stackBarButton = UIBarButtonItem(customView: stackView)
+        navigationItem.rightBarButtonItem = stackBarButton
+    }
+    
+    func setupTableView() {
+        view.addSubview(tableView)
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.separatorStyle = .none
+        tableView.backgroundColor = .clear
+        
+        tableView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+            make.left.right.bottom.equalToSuperview()
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return mockData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ProjectBoardCell", for: indexPath) as! ProjectBoardTableviewCell
+        let dataItem = mockData[indexPath.row]
+        cell.customImageView.image = dataItem.image
+        cell.titleLabel.text = dataItem.title
+        cell.detailLabel.text = dataItem.detail
+        cell.icons = dataItem.icons
+        cell.bindData(image: dataItem.image, title: dataItem.title, detail: dataItem.detail, icons: dataItem.icons, status: dataItem.status)
+        
+        cell.backgroundColor = .clear
+        cell.layer.borderColor = UIColor.black.cgColor
+        cell.contentView.backgroundColor = .white
+        cell.contentView.layer.cornerRadius = 10
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 170
+    }
+    
+    // MARK: - Actions
+    @objc func searchButtonTapped() {
+        // TODO: Implement search action
+    }
+    
+    @objc func bellButtonTapped() {
+        // TODO: Implement bell action
+    }
+    
+    @objc func pencilButtonTapped() {
+        // TODO: Implement pencil action
+    }
+}
