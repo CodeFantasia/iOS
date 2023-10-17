f5c6f21d67b5bcea46abafdef3749a71066091d1
“hw3590” <lkhs44@naver.com>
2023-10-17 17:41:06 +0900

    .feat "내 프로젝트 페이지 TableViewController로 변경"

--- CodeFantasia/MyProjectVC.swift
+++ CodeFantasia/MyProjectVC.swift
@@ -8,17 +8,9 @@
 import UIKit
 import SnapKit
 
-
-class MyProjectVC: UIViewController {
+class MyProjectVC: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
-        let label = UILabel()
-        label.text = "첫 번째 탭"
-        view.addSubview(label)
-        
-        label.snp.makeConstraints { make in
-            make.center.equalToSuperview()
-        }
     }
 }
+
