cca102ecc3b0b5bffb0c96b5ea800e4f1cc67cbc
EyeGrampus <139723584+EyeGrampus@users.noreply.github.com>
2023-10-17 14:17:37 +0900

    feat: 프로젝트 게시판 디테일 뷰

---
+++ CodeFantasia/Resource/Fonts.swift
@@ -0,0 +1,15 @@
+//
+//  Fonts.swift
+//  CodeFantasia
+//
+//  Created by hong on 2023/10/13.
+//
+
+import UIKit
+
+extension UIFont {
+    static let title = systemFont(ofSize: 30, weight: .bold)
+    static let subTitle = systemFont(ofSize: 20, weight: .semibold)
+    static let body = systemFont(ofSize: 16, weight: .regular)
+    static let buttonTitle = systemFont(ofSize: 20, weight: .regular)
+}
