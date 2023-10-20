dad6f291e775401254ed6c9eecf46e00789ab089
Daisy Hong <hdaisywd@gmail.com>
2023-10-20 16:53:30 +0900

    fix: LoginVC UI 화면 앱 내 자체 로그인 화면으로 변경

--- CodeFantasia/LogIn/LoginVC.swift
+++ CodeFantasia/LogIn/LoginVC.swift
@@ -3,6 +3,8 @@ import Foundation
 import UIKit
 
 class LoginVC: UIViewController {
+    private let textfieldHeight = 40
+    
     // MARK: Properties
     private let iconImageView = {
         let imageview = UIImageView()
@@ -13,11 +15,71 @@ class LoginVC: UIViewController {
         return imageview
     }()
     
+    private lazy var loginView = {
+        let containerView = UIView()
+        
+        let idLabel = UILabel()
+        idLabel.text = "아이디"
+        idLabel.textColor = .black
+        idLabel.font = UIFont.subTitle
+        
+        let idTextField = UITextField()
+        idTextField.customConfigure(placeholder: "아이디를 입력해주세요.")
+        
+        let passwordLabel = UILabel()
+        passwordLabel.text = "비밀번호"
+        passwordLabel.textColor = .black
+        passwordLabel.font = UIFont.subTitle
+        
+        let passwordTextField = UITextField()
+        passwordTextField.customConfigure(placeholder: "비밀번호를 입력해주세요.")
+        
+        let loginButton = UIButton()
+        loginButton.primaryColorConfigure(title: "로그인")
+        
+        containerView.addSubview(idLabel)
+        containerView.addSubview(idTextField)
+        containerView.addSubview(passwordLabel)
+        containerView.addSubview(passwordTextField)
+        containerView.addSubview(loginButton)
+        
+        idLabel.snp.makeConstraints { make in
+            make.leading.equalToSuperview()
+            make.top.equalTo(containerView)
+        }
+        
+        idTextField.snp.makeConstraints { make in
+            make.leading.equalTo(idLabel)
+            make.top.equalTo(idLabel.snp.bottom).offset(2)
+            make.height.equalTo(textfieldHeight)
+            make.width.equalTo(containerView)
+        }
+        
+        passwordLabel.snp.makeConstraints { make in
+            make.leading.equalTo(idTextField)
+            make.top.equalTo(idTextField.snp.bottom).offset(2)
+        }
+        
+        passwordTextField.snp.makeConstraints { make in
+            make.leading.equalTo(passwordLabel)
+            make.top.equalTo(passwordLabel.snp.bottom).offset(2)
+            make.height.equalTo(textfieldHeight)
+            make.width.equalTo(containerView)
+        }
+        
+        loginButton.snp.makeConstraints { make in
+            make.top.equalTo(passwordTextField.snp.bottom).offset(20)
+            make.width.equalTo(containerView)
+        }
+        
+        return containerView
+    }()
+    
     private let signupView = {
         let containerView = UIView()
 
         let label = UILabel()
-        label.text = "애플 계정으로 가입하고 이용하세요."
+        label.text = "아이디가 없으신가요?"
         label.textColor = .darkGray
         label.font = UIFont.systemFont(ofSize: CGFloat.subTitle)
         
@@ -26,13 +88,8 @@ class LoginVC: UIViewController {
         button.setTitleColor(UIColor.black, for: .normal)
         button.setTitleColor(UIColor.gray, for: .highlighted)
         
-        let imageview = UIImageView()
-        imageview.image = UIImage(named: "AppleSignUp")
-        imageview.contentMode = .scaleAspectFill
-        
         containerView.addSubview(label)
         containerView.addSubview(button)
-        containerView.addSubview(imageview)
         
         label.snp.makeConstraints { make in
             make.leading.top.equalToSuperview()
@@ -40,14 +97,8 @@ class LoginVC: UIViewController {
         }
         
         button.snp.makeConstraints { make in
-            make.leading.equalTo(label.snp.trailing).offset(2)
+            make.leading.equalTo(label.snp.trailing).offset(3)
             make.centerY.equalTo(label)
-        }
-        
-        imageview.snp.makeConstraints { make in
-            make.centerX.equalTo(containerView)
-            make.top.equalTo(button.snp.bottom).offset(5)
-            make.width.equalToSuperview()
         }
         
         return containerView
@@ -63,17 +114,24 @@ class LoginVC: UIViewController {
     // MARK: Layer Configuration
     private func layerConfiguration() {
         view.addSubview(iconImageView)
+        view.addSubview(loginView)
         view.addSubview(signupView)
         
         iconImageView.snp.makeConstraints { make in
             make.top.equalTo(view.snp.top).offset(50)
             make.leading.trailing.equalToSuperview().inset(CGFloat.spacing)
-            make.height.equalTo(500)
+            make.height.equalTo(400)
+        }
+        
+        loginView.snp.makeConstraints { make in
+            make.top.equalTo(iconImageView.snp.bottom).offset(20)
+            make.leading.trailing.equalToSuperview().inset(CGFloat.spacing)
+            make.height.equalTo(200)
         }
         
         signupView.snp.makeConstraints { make in
-            make.top.equalTo(iconImageView.snp.bottom).offset(20)
-            make.leading.trailing.equalToSuperview().inset(40)
+            make.top.equalTo(loginView.snp.bottom).offset(0)
+            make.leading.trailing.equalTo(loginView)
             make.height.equalTo(200)
         }
     }
