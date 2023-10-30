f3da033f552c108f5fc68066bc39b95e2a1d4d7d
Daisy Hong <hdaisywd@gmail.com>
2023-10-30 14:50:34 +0900

    feat: Profile image URL 등록 가능

--- CodeFantasia/Feature/UserManager.swift
+++ CodeFantasia/Feature/UserManager.swift
@@ -1,8 +1,10 @@
 
 import Firebase
+import FirebaseDatabase
+import FirebaseStorage
 
-let DB_REF = Database.database().referece() 
+let DB_REF = Database.database().reference()
 let REF_USERS = DB_REF.child("users")
 
-let STORAGE_REF = Storage.stroage().reference()
+let STORAGE_REF = Storage.storage().reference()
 let STORAGE_PROFILE_IMAGES = STORAGE_REF.child("profile_images")
