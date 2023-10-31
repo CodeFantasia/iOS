d80bb1f1aa941259c9428699f76c1455a284656e
Daisy Hong <hdaisywd@gmail.com>
2023-11-01 03:37:51 +0900

    feat: 프로필 사진 등록하지 않을시 디폴트 사진 넣어주는 기능 추가

--- CodeFantasia/Controller/Authentication/SignUp/RegistrationController.swift
+++ CodeFantasia/Controller/Authentication/SignUp/RegistrationController.swift
@@ -29,6 +29,14 @@ class RegistrationController: UIViewController {
         return view
     }()
     
+    private let emailCheckFailed: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.smallTitle
+        label.text = "올바른 이메일 형식이 아닙니다."
+        label.textColor = .systemRed
+        return label
+    }()
+    
     private lazy var passwordContainerView: UIView = {
         let view = Utilities().inputContainerView(withImage: UIImage(named: "ic_lock_outline_white_2x")!, textField: passwordTextField)
         return view
@@ -47,11 +55,25 @@ class RegistrationController: UIViewController {
         return view
     }()
     
-    
+    private let passwordDoNotMatch: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.smallTitle
+        label.text = "비밀번호가 일치하지 않습니다."
+        label.textColor = .systemRed
+        return label
+    }()
     
     private lazy var nameContainerView: UIView = {
         let view = Utilities().inputContainerView(withImage: UIImage(named: "ic_person_outline_white_2x")!, textField: nameTextField)
         return view
+    }()
+    
+    private let nameCheckFailed: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.smallTitle
+        label.text = "영어와 한국어로 이름을 작성해 주세요."
+        label.textColor = .systemRed
+        return label
     }()
     
     private let emailTextField: UITextField = {
@@ -61,6 +83,7 @@ class RegistrationController: UIViewController {
     
     private let passwordTextField: UITextField = {
         let textfield = Utilities().textField(withPlaceholder: "Password")
+        textfield.textContentType = .oneTimeCode
         textfield.isSecureTextEntry = true
         return textfield
     }()
@@ -73,6 +96,7 @@ class RegistrationController: UIViewController {
     private let passwordCheckTextField: UITextField = {
         let textfield = Utilities().textField(withPlaceholder: "Re-enter your password")
         textfield.isSecureTextEntry = true
+        textfield.textContentType = .oneTimeCode
         return textfield
     }()
     
@@ -106,33 +130,12 @@ class RegistrationController: UIViewController {
     }
     
     @objc func handleRegister() {
-        guard let profileImage = profileImage else {
-            print("프로필 사진이 선택되지 않았습니다.")
-            return
-        }
+        let profileImage = profileImage ?? UIImage(named: "default_profile")
+        guard let email = emailTextField.text, emailVerify(email: email) else { return }
+        guard let password = passwordTextField.text, let passwordCheck = passwordCheckTextField.text, passwordVerify(password: password, passwordMatch: passwordCheck) else { return }
+        guard let name = nameTextField.text, nameVerify(name: name) else { return }
         
-        guard let email = emailTextField.text else { return }
-        guard let password = passwordTextField.text else { return }
-        
-        if !passwordCheck(password: password) {
-            print("경고 경고 비밀번호 이상이상")
-            passwordContainerView.shake()
-            passwordCheckFailed.isHidden = false
-            return
-        } else {
-            passwordCheckFailed.isHidden = true
-        }
-        
-        guard let passwordCheck = passwordCheckTextField.text else { return }
-        
-        if password != passwordCheck {
-            print("경고 경고 비밀번호 일치하지 않음")
-            return
-        }
-        
-        guard let name = nameTextField.text else { return }
-        
-        let newUser = UserAuth(email: email, password: password, name: name, profileImage: profileImage)
+        let newUser = UserAuth(email: email, password: password, name: name, profileImage: profileImage ?? UIImage())
         
         AuthManager.shared.registerUser(crudentials: newUser) { (error, ref) in
             guard let window = UIApplication.shared.windows.first(where: { $0.isKeyWindow }) else { return }
@@ -149,6 +152,49 @@ class RegistrationController: UIViewController {
     }
     
     // MARK: - Helpers
+    
+    func emailVerify(email: String) -> Bool {
+        if !emailCheck(email) {
+            checkFailed(shakeView: emailContainerView, alertLabel: emailCheckFailed)
+            return false
+        } else {
+            emailCheckFailed.isHidden = true
+            return true
+        }
+    }
+    
+    func passwordVerify(password: String, passwordMatch: String) -> Bool {
+        if !passwordCheck(password: password) {
+            checkFailed(shakeView: passwordContainerView, alertLabel: passwordCheckFailed)
+            return false
+        } else {
+            passwordCheckFailed.isHidden = true
+        }
+
+        if password != passwordMatch {
+            checkFailed(shakeView: passwordCheckContainerView, alertLabel: passwordDoNotMatch)
+            return false
+        } else {
+            passwordDoNotMatch.isHidden = true
+        }
+
+        return true
+    }
+    
+    func nameVerify(name: String) -> Bool {
+        if !containsOnlyEnglishAndKorean(name) || name.count < 1 {
+            checkFailed(shakeView: nameContainerView, alertLabel: nameCheckFailed)
+            return false
+        } else {
+            nameCheckFailed.isHidden = true
+            return true
+        }
+    }
+    
+    func checkFailed(shakeView: UIView, alertLabel: UILabel) {
+        shakeView.shake()
+        alertLabel.isHidden = false
+    }
     
     func configureUI() {
         view.backgroundColor = .primaryColor
@@ -169,7 +215,7 @@ class RegistrationController: UIViewController {
             make.height.width.equalTo(150)
         }
 
-        let stack = UIStackView(arrangedSubviews: [emailContainerView, passwordContainerView, passwordCheckContainerView, nameContainerView, registrationButton])
+        let stack = UIStackView(arrangedSubviews: [emailContainerView, passwordContainerView, passwordCheckContainerView, nameContainerView])
         stack.axis = .vertical
         stack.spacing = 20
         stack.distribution = .fillEqually
@@ -180,15 +226,42 @@ class RegistrationController: UIViewController {
             make.left.right.equalToSuperview().inset(8)
         }
         
+        emailCheckFailed.isHidden = true
+        view.addSubview(emailCheckFailed)
+        emailCheckFailed.snp.makeConstraints { make in
+            make.top.equalTo(emailContainerView.snp.bottom).offset(5)
+            make.left.equalTo(emailContainerView).offset(5)
+        }
+        
         passwordCheckFailed.isHidden = true
         view.addSubview(passwordCheckFailed)
         passwordCheckFailed.snp.makeConstraints { make in
             make.top.equalTo(passwordContainerView.snp.bottom).offset(5)
-            make.left.equalTo(passwordContainerView).offset(2)
+            make.left.equalTo(passwordContainerView).offset(5)
+        }
+        
+        passwordDoNotMatch.isHidden = true
+        view.addSubview(passwordDoNotMatch)
+        passwordDoNotMatch.snp.makeConstraints { make in
+            make.top.equalTo(passwordCheckContainerView.snp.bottom).offset(5)
+            make.left.equalTo(passwordCheckContainerView).offset(5)
+        }
+        
+        nameCheckFailed.isHidden = true
+        view.addSubview(nameCheckFailed)
+        nameCheckFailed.snp.makeConstraints { make in
+            make.top.equalTo(nameContainerView.snp.bottom).offset(5)
+            make.left.equalTo(nameContainerView).offset(5)
+        }
+        
+        view.addSubview(registrationButton)
+        registrationButton.snp.makeConstraints { make in
+            make.top.equalTo(nameContainerView.snp.bottom).offset(45)
+            make.left.right.equalTo(stack)
         }
     }
     
-    // MARK: - Password Check
+    // MARK: - Email, Password, Name check
     func passwordCheck(password: String) -> Bool {
         if (password.count < 6 || password.count > 20) || !containsAlphanumeric(password) {
                 return false
@@ -201,7 +274,16 @@ class RegistrationController: UIViewController {
         let alphanumericPattern = "^(?=.*[A-Za-z])(?=.*\\d).{6,20}$"
         return NSPredicate(format: "SELF MATCHES %@", alphanumericPattern).evaluate(with: input)
     }
-
+    
+    func emailCheck(_ input: String) -> Bool {
+        let emailPattern = "^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\\.[a-zA-Z]{2,3}$"
+        return NSPredicate(format: "SELF MATCHES %@", emailPattern).evaluate(with: input)
+    }
+    
+    func containsOnlyEnglishAndKorean(_ input: String) -> Bool {
+        let pattern = "^[a-zA-Z가-힣]*$"
+        return NSPredicate(format: "SELF MATCHES %@", pattern).evaluate(with: input)
+    }
 }
 
 // MARK: - UIImagePickerControllerDelegate
