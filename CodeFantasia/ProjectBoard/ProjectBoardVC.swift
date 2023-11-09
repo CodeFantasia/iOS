798522964acc733c3346b313dc7562ee34f37adf
YoungDuck1592 <tjdudejr1919@gmail.com>
2023-11-09 16:01:06 +0900

    Design: " 테이블 셀 디자인 변경"

--- CodeFantasia/ProjectBoard/ProjectBoardVC.swift
+++ CodeFantasia/ProjectBoard/ProjectBoardVC.swift
@@ -1,8 +1,8 @@
 //
-//  ProjectBoardVC.swift
-//  CodeFantasia
+// ProjectBoardVC.swift
+// CodeFantasia
 //
-//  Created by 서영덕 on 10/13/23.
+// Created by 서영덕 on 10/13/23.
 //
 
 import UIKit
@@ -13,13 +13,13 @@ import Firebase
 import FirebaseAuth
 
 class ProjectBoardVC: UIViewController {
-    
     var blockIds: [String]?
     var writerID: String?
-    
     private let tableView: UITableView = UITableView().then {
         $0.register(ProjectBoardTableviewCell.self, forCellReuseIdentifier: "ProjectBoardCell")
-        $0.separatorStyle = .none
+        $0.separatorStyle = .singleLine // 이 부분을 .none에서 .singleLine으로 변경합니다.
+        $0.separatorColor = UIColor.gray // 구분선의 색상을 설정합니다.
+        $0.separatorInset = UIEdgeInsets(top: 0, left: 15, bottom: 0, right: 15) // 구분선의 여백을 설정합니다.
         $0.backgroundColor = .clear
     }
     
@@ -28,35 +28,30 @@ class ProjectBoardVC: UIViewController {
     private var projectsData: [(imageURL: URL?, title: String, detail: String, icons: [IconModel], status: String, projectID: UUID)] = []
     private var bag = DisposeBag()
     
-    
     private func fetchDataFromFirebase() {
-
-
         ////////////////////
         if let currentUser = Auth.auth().currentUser?.uid {
-        let db = Firestore.firestore()
-
-        // Firestore에서 현재 사용자의 문서 가져오기
-        let userDocumentReference = db.collection("User").document(currentUser)
-
-        userDocumentReference.getDocument { (snapshot, error) in
-        if let error = error {
-        print("Error fetching document: \(error)")
-        } else if let snapshot = snapshot, snapshot.exists {
-        // 문서가 존재하면 필드 값을 가져올 수 있습니다.
-        if let data = snapshot.data(),
-        let blockIDs = data["blockIDs"] as? [String] {
-        // blockIDs 필드가 배열인 경우, [String]로 타입 캐스트
-        print("blockIDs: \(blockIDs)")
-            self.blockIds = blockIDs
-        // blockIDs 값을 projectRepository.readAll 함수로 전달
-        } else {
-        print("blockIDs field not found or has the wrong type")
-        }
-        } else {
-        print("Document does not exist")
-        }
-        }
+            let db = Firestore.firestore()
+            // Firestore에서 현재 사용자의 문서 가져오기
+            let userDocumentReference = db.collection("User").document(currentUser)
+            userDocumentReference.getDocument { (snapshot, error) in
+                if let error = error {
+                    print("Error fetching document: \(error)")
+                } else if let snapshot = snapshot, snapshot.exists {
+                    // 문서가 존재하면 필드 값을 가져올 수 있습니다.
+                    if let data = snapshot.data(),
+                       let blockIDs = data["blockIDs"] as? [String] {
+                        // blockIDs 필드가 배열인 경우, [String]로 타입 캐스트
+                        print("blockIDs: \(blockIDs)")
+                        self.blockIds = blockIDs
+                        // blockIDs 값을 projectRepository.readAll 함수로 전달
+                    } else {
+                        print("blockIDs field not found or has the wrong type")
+                    }
+                } else {
+                    print("Document does not exist")
+                }
+            }
         }
         ///
         ///
@@ -72,7 +67,6 @@ class ProjectBoardVC: UIViewController {
                             return IconModel(image: UIImage(named: techName) ?? UIImage())
                         }
                     }
-
                     return (imageURL, project.projectTitle ?? "", project.projectDescription ?? "", icons, statusString, project.projectID)
                 }
                 self?.tableView.reloadData()
@@ -83,34 +77,29 @@ class ProjectBoardVC: UIViewController {
             })
             .disposed(by: bag)
     }
-
-// MARK: - ViewController Lifecycle
+    
+    // MARK: - ViewController Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         refreshControl.addTarget(self, action: #selector(handleRefresh), for: .valueChanged)
         tableView.addSubview(refreshControl)
-        
         self.fetchDataFromFirebase()
-        
         setupNavigationBar()
         setupTableView()
         setupFloatingActionButton()
-        
-        view.backgroundColor = UIColor(red: 245/255.0, green: 245/255.0, blue: 245/255.0, alpha: 1.0)
-        navigationController?.navigationBar.backgroundColor = UIColor(red: 245/255.0, green: 245/255.0, blue: 245/255.0, alpha: 1.0)
+        view.backgroundColor = .white
+        navigationController?.navigationBar.backgroundColor = .white
         navigationController?.navigationBar.tintColor = .black
     }
-    
     @objc private func handleRefresh() {
         fetchDataFromFirebase()
         refreshControl.endRefreshing()
     }
-    
 }
 
 // MARK: - TableView DataSource & Delegate
+
 extension ProjectBoardVC: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return projectsData.count
@@ -119,7 +108,6 @@ extension ProjectBoardVC: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ProjectBoardCell", for: indexPath) as! ProjectBoardTableviewCell
         let dataItem = projectsData[indexPath.row]
-        
         if let imageURL = dataItem.imageURL {
             URLSession.shared.dataTask(with: imageURL) { (data, response, error) in
                 if let data = data, let image = UIImage(data: data) {
@@ -134,15 +122,10 @@ extension ProjectBoardVC: UITableViewDataSource, UITableViewDelegate {
         cell.subheadingLabel.text = dataItem.detail
         cell.icons = dataItem.icons
         cell.recruitmentLabelCheck(imageURL: dataItem.imageURL, title: dataItem.title, detail: dataItem.detail, icons: dataItem.icons, status: dataItem.status)
-        
         cell.backgroundColor = .clear
-        cell.layer.borderColor = UIColor.black.cgColor
-        cell.contentView.backgroundColor = .white
-        cell.contentView.layer.cornerRadius = 10
-        
+        cell.contentView.backgroundColor = .clear
         return cell
     }
-    
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 170
@@ -150,35 +133,27 @@ extension ProjectBoardVC: UITableViewDataSource, UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
-        
         let selectedProject = projectsData[indexPath.row]
-        
         let projectId = selectedProject.5.uuidString
-        
         let viewModel = ProjectDetailNoticeBoardViewModel(projectRepository: projectRepository, projectId: projectId)
-        
         let detailVC = ProjectDetailNoticeBoardViewController(viewModel: viewModel)
-        
         navigationController?.pushViewController(detailVC, animated: true)
-        
         print(projectId)
         print(selectedProject.projectID)
     }
-    
 }
 
 // MARK: - Actions & Event Handlers (추후에 분리)
+
 extension ProjectBoardVC {
     @objc func searchButtonTapped() {
         _ = projectsData.map { (image: $0.0, title: $0.1, detail: $0.2, icons: $0.3, status: $0.4) }
         let searchVC = ProjectSearchViewVC(mockData: projectsData)
         navigationController?.pushViewController(searchVC, animated: true)
     }
-    
-//    @objc func bellButtonTapped() {
-//        // TODO: Implement bell action
-//    }
-    
+    //  @objc func bellButtonTapped() {
+    //    // TODO: Implement bell action
+    //  }
     @objc func plusButtonTapped() {
         let newPageVC = NewPageViewController(data: nil)
         newPageVC.modalPresentationStyle = .fullScreen
@@ -187,33 +162,31 @@ extension ProjectBoardVC {
 }
 
 // MARK: - UI Setup
+
 private extension ProjectBoardVC {
     func setupNavigationBar() {
-        let customTitleLabel = UILabel().then {
-            $0.text = "프로젝트 게시판"
-            $0.font = UIFont.boldSystemFont(ofSize: 24)
-            $0.textColor = .black
+        let logoImageView = UIImageView().then {
+            $0.contentMode = .scaleAspectFit
+            $0.image = UIImage(named: "AppIcon_long") // 에셋에서 "AppIcon" 이름의 이미지를 사용합니다.
+            $0.widthAnchor.constraint(equalToConstant: 100).isActive = true // 로고 이미지의 너비를 설정합니다.
+            $0.heightAnchor.constraint(equalToConstant: 50).isActive = true // 로고 이미지의 높이를 설정합니다.
         }
         
-        let customTitleBarItem = UIBarButtonItem(customView: customTitleLabel)
-        navigationItem.leftBarButtonItem = customTitleBarItem
-        
+        let logoBarItem = UIBarButtonItem(customView: logoImageView)
+        navigationItem.leftBarButtonItem = logoBarItem
         let searchButtonView = UIButton(type: .system).then {
             $0.setImage(UIImage(systemName: "magnifyingglass"), for: .normal)
             $0.addTarget(self, action: #selector(searchButtonTapped), for: .touchUpInside)
         }
-        
-//        let bellButtonView = UIButton(type: .system).then {
-//            $0.setImage(UIImage(systemName: "bell"), for: .normal)
-//            $0.addTarget(self, action: #selector(bellButtonTapped), for: .touchUpInside)
-//        }
-        
-//        let pencilButtonView = UIButton(type: .system).then {
-//            $0.setImage(UIImage(systemName: "pencil"), for: .normal)
-//            $0.addTarget(self, action: #selector(plusButtonTapped), for: .touchUpInside)
-//        }
-        
-        let stackView = UIStackView(arrangedSubviews: [searchButtonView/*, bellButtonView*//*, pencilButtonView*/]).then {
+        //    let bellButtonView = UIButton(type: .system).then {
+        //      $0.setImage(UIImage(systemName: "bell"), for: .normal)
+        //      $0.addTarget(self, action: #selector(bellButtonTapped), for: .touchUpInside)
+        //    }
+        //    let pencilButtonView = UIButton(type: .system).then {
+        //      $0.setImage(UIImage(systemName: "pencil"), for: .normal)
+        //      $0.addTarget(self, action: #selector(plusButtonTapped), for: .touchUpInside)
+        //    }
+        let stackView = UIStackView(arrangedSubviews: [searchButtonView/*, bellButtonView, pencilButtonView*/]).then {
             $0.axis = .horizontal
             $0.spacing = 10
         }
@@ -222,13 +195,10 @@ private extension ProjectBoardVC {
         navigationItem.rightBarButtonItem = stackBarButton
     }
     
-    
-    
     func setupTableView() {
         view.addSubview(tableView)
         tableView.dataSource = self
         tableView.delegate = self
-        
         tableView.snp.makeConstraints { make in
             make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
             make.left.right.bottom.equalToSuperview()
@@ -236,20 +206,20 @@ private extension ProjectBoardVC {
     }
     
     func setupFloatingActionButton() {
-        let plusButtonView = UIButton(type: .custom).then {
+        let plusButtonView = UIButton().then {
             $0.setImage(UIImage(systemName: "plus"), for: .normal)
-            $0.tintColor = .black
+            $0.tintColor = .white
             $0.addTarget(self, action: #selector(plusButtonTapped), for: .touchUpInside)
             $0.primaryColorConfigure(title: "")
-            $0.layer.cornerRadius = 28
+            $0.layer.cornerRadius = 30
+            $0.backgroundColor = .black
         }
-
+        
         view.addSubview(plusButtonView)
         plusButtonView.snp.makeConstraints { make in
             make.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom).offset(-16)
             make.right.equalToSuperview().offset(-16)
-            make.width.height.equalTo(56)
+            make.width.height.equalTo(60)
         }
     }
 }
-
--- CodeFantasia/ProjectBoard/ProjectSearchViewVC.swift
+++ CodeFantasia/ProjectBoard/ProjectSearchViewVC.swift
@@ -1,218 +1,178 @@
 //
-//  ProjectSearchViewVC.swift
-//  CodeFantasia
+// ProjectSearchViewVC.swift
+// CodeFantasia
 //
-//  Created by 서영덕 on 10/23/23.
+// Created by 서영덕 on 10/23/23.
 //
-
 import UIKit
 import SnapKit
 import Then
 import RxSwift
 
-
-
 class ProjectSearchViewVC: UIViewController, UISearchResultsUpdating, UISearchBarDelegate, UITableViewDataSource, UITableViewDelegate, UIViewControllerTransitioningDelegate, FilterBottomSheetDelegate {
     
-    private let tableView = UITableView().then {
-        $0.register(ProjectBoardTableviewCell.self, forCellReuseIdentifier: "ProjectBoardCell")
-        $0.separatorStyle = .none
-        $0.backgroundColor = .clear
+  private let tableView: UITableView = UITableView().then {
+    $0.register(ProjectBoardTableviewCell.self, forCellReuseIdentifier: "ProjectBoardCell")
+    $0.separatorStyle = .singleLine
+    $0.separatorColor = UIColor.gray
+    $0.separatorInset = UIEdgeInsets(top: 0, left: 15, bottom: 0, right: 15)
+    $0.backgroundColor = .clear
+  }
+    
+  private var projectsData: [(imageURL: URL?, title: String, detail: String, icons: [IconModel], status: String, projectID: UUID)] = []
+  private let projectRepository: ProjectRepositoryProtocol = ProjectRepository(firebaseBaseManager: FireBaseManager())
+  private var filteredData: [(imageURL: URL?, title: String, detail: String, icons: [IconModel], status: String, projectID: UUID)]
+  private let searchController = UISearchController(searchResultsController: nil)
+  var mockData: [(imageURL: URL?, title: String, detail: String, icons: [IconModel], status: String, projectID: UUID)]
+  init(mockData: [(imageURL: URL?, title: String, detail: String, icons: [IconModel], status: String, projectID: UUID)]) {
+    self.mockData = mockData
+    self.filteredData = mockData
+    super.init(nibName: nil, bundle: nil)
+  }
+    
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+    
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .white
+    navigationController?.navigationBar.backgroundColor = .white
+    navigationItem.searchController = searchController
+    navigationItem.hidesSearchBarWhenScrolling = false
+    if let filterImage = UIImage(systemName: "slider.vertical.3") {
+      let filterButton = UIBarButtonItem(image: filterImage, style: .plain, target: self, action: #selector(filterButtonTapped))
+      navigationItem.rightBarButtonItem = filterButton
     }
+    setupUI()
+  }
     
-    private var projectsData: [(imageURL: URL?, title: String, detail: String, icons: [IconModel], status: String, projectID: UUID)] = []
-    private let projectRepository: ProjectRepositoryProtocol = ProjectRepository(firebaseBaseManager: FireBaseManager())
-    
-    
-    private var filteredData: [(imageURL: URL?, title: String, detail: String, icons: [IconModel], status: String, projectID: UUID)]
-    private let searchController = UISearchController(searchResultsController: nil)
-    var mockData: [(imageURL: URL?, title: String, detail: String, icons: [IconModel], status: String, projectID: UUID)]
-    
-    init(mockData: [(imageURL: URL?, title: String, detail: String, icons: [IconModel], status: String, projectID: UUID)]) {
-        self.mockData = mockData
-        self.filteredData = mockData
-        super.init(nibName: nil, bundle: nil)
+  @objc func filterButtonTapped() {
+    let filterVC = FilterBottomSheetVC()
+    filterVC.delegate = self
+    filterVC.modalPresentationStyle = .custom
+    filterVC.transitioningDelegate = self
+    self.present(filterVC, animated: true, completion: nil)
+  }
+  func didApplyFilter(showOngoingProjectsOnly: Bool) {
+    if showOngoingProjectsOnly {
+      filteredData = mockData.filter { $0.status == "모집 중" }
+    } else {
+      filteredData = mockData
     }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+    tableView.reloadData()
+  }
+  private func setupUI() {
+    setupSearchController()
+    setupTableView()
+  }
+  private func setupSearchController() {
+    searchController.searchResultsUpdater = self
+    searchController.obscuresBackgroundDuringPresentation = false
+    searchController.searchBar.placeholder = "제목 검색"
+    definesPresentationContext = true
+    searchController.hidesNavigationBarDuringPresentation = false
+    searchController.searchBar.delegate = self
+  }
+  private func setupTableView() {
+    view.addSubview(tableView)
+    tableView.dataSource = self
+    tableView.delegate = self
+    tableView.snp.makeConstraints { make in
+      make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+      make.left.right.bottom.equalToSuperview()
     }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .white
-        
-        navigationController?.navigationBar.backgroundColor = .white
-        
-        navigationItem.searchController = searchController
-        navigationItem.hidesSearchBarWhenScrolling = false
-        
-        if let filterImage = UIImage(systemName: "slider.vertical.3") {
-            let filterButton = UIBarButtonItem(image: filterImage, style: .plain, target: self, action: #selector(filterButtonTapped))
-            navigationItem.rightBarButtonItem = filterButton
-        }
-        
-        setupUI()
+  }
+  func presentationController(forPresented presented: UIViewController, presenting: UIViewController?, source: UIViewController) -> UIPresentationController? {
+    return HalfSizePresentationController(presentedViewController: presented, presenting: presenting)
+  }
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return filteredData.count
+  }
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "ProjectBoardCell", for: indexPath) as! ProjectBoardTableviewCell
+    let dataItem = filteredData[indexPath.row]
+    cell.recruitmentLabelCheck(imageURL: dataItem.imageURL, title: dataItem.title, detail: dataItem.detail, icons: dataItem.icons, status: dataItem.status)
+    cell.backgroundColor = .clear
+    cell.layer.borderColor = UIColor.black.cgColor
+    cell.contentView.backgroundColor = .white
+    cell.contentView.layer.cornerRadius = 10
+    return cell
+  }
+  func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    return 170
+  }
+  func updateSearchResults(for searchController: UISearchController) {
+    //  if let searchText = searchController.searchBar.text, !searchText.isEmpty {
+    //   filteredData = mockData.filter { $0.title.contains(searchText) }
+    //  } else {
+    //   filteredData = mockData
+    //  }
+    //  tableView.reloadData()
+  }
+  func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+    if let searchText = searchBar.text, !searchText.isEmpty {
+      filteredData = mockData.filter { $0.title.contains(searchText) }
+    } else {
+      filteredData = mockData
     }
-    
-    @objc func filterButtonTapped() {
-        let filterVC = FilterBottomSheetVC()
-        filterVC.delegate = self
-        filterVC.modalPresentationStyle = .custom
-        filterVC.transitioningDelegate = self
-        self.present(filterVC, animated: true, completion: nil)
-    }
-    
-    func didApplyFilter(showOngoingProjectsOnly: Bool) {
-        if showOngoingProjectsOnly {
-            filteredData = mockData.filter { $0.status == "모집 중" }
-        } else {
-            filteredData = mockData
-        }
-        tableView.reloadData()
-    }
-    
-    private func setupUI() {
-        setupSearchController()
-        setupTableView()
-    }
-    
-    private func setupSearchController() {
-        searchController.searchResultsUpdater = self
-        searchController.obscuresBackgroundDuringPresentation = false
-        searchController.searchBar.placeholder = "제목 검색"
-        definesPresentationContext = true
-        searchController.hidesNavigationBarDuringPresentation = false
-        searchController.searchBar.delegate = self
-    }
-    
-    private func setupTableView() {
-        view.addSubview(tableView)
-        tableView.dataSource = self
-        tableView.delegate = self
-        tableView.snp.makeConstraints { make in
-            make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
-            make.left.right.bottom.equalToSuperview()
-        }
-    }
-    
-    func presentationController(forPresented presented: UIViewController, presenting: UIViewController?, source: UIViewController) -> UIPresentationController? {
-        return HalfSizePresentationController(presentedViewController: presented, presenting: presenting)
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return filteredData.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "ProjectBoardCell", for: indexPath) as! ProjectBoardTableviewCell
-        let dataItem = filteredData[indexPath.row]
-        
-        cell.recruitmentLabelCheck(imageURL: dataItem.imageURL, title: dataItem.title, detail: dataItem.detail, icons: dataItem.icons, status: dataItem.status)
-        
-        cell.backgroundColor = .clear
-        cell.layer.borderColor = UIColor.black.cgColor
-        cell.contentView.backgroundColor = .white
-        cell.contentView.layer.cornerRadius = 10
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 170
-    }
-    
-    func updateSearchResults(for searchController: UISearchController) {
-        //    if let searchText = searchController.searchBar.text, !searchText.isEmpty {
-        //      filteredData = mockData.filter { $0.title.contains(searchText) }
-        //    } else {
-        //      filteredData = mockData
-        //    }
-        //    tableView.reloadData()
-    }
-    
-    
-    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        if let searchText = searchBar.text, !searchText.isEmpty {
-            filteredData = mockData.filter { $0.title.contains(searchText) }
-        } else {
-            filteredData = mockData
-        }
-        tableView.reloadData()
-    }
+    tableView.reloadData()
+  }
 }
-
 class HalfSizePresentationController: UIPresentationController {
-    private var dimmingView: UIView?
-    private var tapGestureRecognizer: UITapGestureRecognizer?
-    
-    override var frameOfPresentedViewInContainerView: CGRect {
-        guard let containerView = containerView else { return .zero }
-        return CGRect(x: 0, y: containerView.bounds.height / 2, width: containerView.bounds.width, height: containerView.bounds.height / 2)
+  private var dimmingView: UIView?
+  private var tapGestureRecognizer: UITapGestureRecognizer?
+  override var frameOfPresentedViewInContainerView: CGRect {
+    guard let containerView = containerView else { return .zero }
+    return CGRect(x: 0, y: containerView.bounds.height / 2, width: containerView.bounds.width, height: containerView.bounds.height / 2)
+  }
+  override func presentationTransitionWillBegin() {
+    guard let containerView = containerView else { return }
+    dimmingView = UIView(frame: containerView.bounds)
+    dimmingView?.backgroundColor = UIColor.black.withAlphaComponent(0.5)
+    dimmingView?.alpha = 0.0
+    containerView.insertSubview(dimmingView!, at: 0)
+    tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
+    dimmingView?.addGestureRecognizer(tapGestureRecognizer!)
+    guard let coordinator = presentedViewController.transitionCoordinator else {
+      dimmingView?.alpha = 1.0
+      return
     }
-    
-    override func presentationTransitionWillBegin() {
-        guard let containerView = containerView else { return }
-        
-        dimmingView = UIView(frame: containerView.bounds)
-        dimmingView?.backgroundColor = UIColor.black.withAlphaComponent(0.5)
-        dimmingView?.alpha = 0.0
-        containerView.insertSubview(dimmingView!, at: 0)
-        
-        tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
-        dimmingView?.addGestureRecognizer(tapGestureRecognizer!)
-        
-        guard let coordinator = presentedViewController.transitionCoordinator else {
-            dimmingView?.alpha = 1.0
-            return
-        }
-        
-        coordinator.animate { _ in
-            self.dimmingView?.alpha = 1.0
-        }
+    coordinator.animate { _ in
+      self.dimmingView?.alpha = 1.0
     }
-    
-    override func dismissalTransitionWillBegin() {
-        guard let coordinator = presentedViewController.transitionCoordinator else {
-            dimmingView?.alpha = 0.0
-            return
-        }
-        
-        coordinator.animate { _ in
-            self.dimmingView?.alpha = 0.0
-        }
+  }
+  override func dismissalTransitionWillBegin() {
+    guard let coordinator = presentedViewController.transitionCoordinator else {
+      dimmingView?.alpha = 0.0
+      return
     }
-    
-    override func dismissalTransitionDidEnd(_ completed: Bool) {
-        if completed {
-            dimmingView?.removeFromSuperview()
-            dimmingView = nil
-        }
+    coordinator.animate { _ in
+      self.dimmingView?.alpha = 0.0
     }
-    
-    @objc func handleTap(_ gesture: UITapGestureRecognizer) {
-        presentedViewController.dismiss(animated: true)
+  }
+  override func dismissalTransitionDidEnd(_ completed: Bool) {
+    if completed {
+      dimmingView?.removeFromSuperview()
+      dimmingView = nil
     }
+  }
+  @objc func handleTap(_ gesture: UITapGestureRecognizer) {
+    presentedViewController.dismiss(animated: true)
+  }
 }
-
 // MARK: - TableView Delegate
 extension ProjectSearchViewVC {
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.deselectRow(at: indexPath, animated: true)
-        
-        guard indexPath.row < filteredData.count else {
-            print("Selected index is out of range.")
-            return
-        }
-
-        let selectedProject = filteredData[indexPath.row]
-        
-        let projectId = selectedProject.5.uuidString
-        
-        let viewModel = ProjectDetailNoticeBoardViewModel(projectRepository: projectRepository, projectId: projectId)
-        
-        let detailVC = ProjectDetailNoticeBoardViewController(viewModel: viewModel)
-        
-        navigationController?.pushViewController(detailVC, animated: true)
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    tableView.deselectRow(at: indexPath, animated: true)
+    guard indexPath.row < filteredData.count else {
+      print("Selected index is out of range.")
+      return
     }
+    let selectedProject = filteredData[indexPath.row]
+    let projectId = selectedProject.5.uuidString
+    let viewModel = ProjectDetailNoticeBoardViewModel(projectRepository: projectRepository, projectId: projectId)
+    let detailVC = ProjectDetailNoticeBoardViewController(viewModel: viewModel)
+    navigationController?.pushViewController(detailVC, animated: true)
+  }
 }
-
