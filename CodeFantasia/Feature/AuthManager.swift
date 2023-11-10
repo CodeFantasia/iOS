f51469b38de9c075e256c01584236ca8d1f15a56
“hw3590” <lkhs44@naver.com>
2023-11-11 01:58:21 +0900

    fix. 실시간 데이터 베이스에 데이터를 추가하지 않고 덮어쓰는 문제 해결 및 이메일 형식에 대문자는 사용하지 못하도록 수정

--- CodeFantasia/Feature/AuthManager.swift
+++ CodeFantasia/Feature/AuthManager.swift
@@ -15,53 +15,63 @@ struct AuthManager {
     static let shared = AuthManager()
     
     func registerUser(crudentials: UserAuth, completion: @escaping(Error?, DatabaseReference) -> Void) {
-        let email = crudentials.email
+        let email = crudentials.email.lowercased()
         let password = crudentials.password
         
-        Auth.auth().createUser(withEmail: email, password: password)
-        DB_REF.child("emails").setValue(["email" : email])
+        Auth.auth().createUser(withEmail: email, password: password) { (authResult, error) in
+            if let error = error {
+                completion(error, DatabaseReference())
+            } else if let authResult = authResult {
+                let uid = authResult.user.uid
+                DB_REF.child("emails").child(uid).setValue(["email": email], withCompletionBlock: { (error, ref) in
+                    completion(error, ref)
+                })
+            } else {
+                let unexpectedError = NSError(domain: "YourDomain", code: 500, userInfo: nil)
+                completion(unexpectedError, DatabaseReference())
+            }
+        }
     }
-    
+
     func checkDuplicate(email: String, completion: @escaping (Bool) -> Void) {
         DB_REF.child("emails").observeSingleEvent(of: .value) { (snapshot) in
             if snapshot.exists() {
-                if let emails = snapshot.value as? [String: Any],
-                   let storedEmail = emails["email"] as? String {
-                    if storedEmail == email {
-                        completion(false)
-                    } else {
-                        completion(true)
+                if let emailsSnapshot = snapshot.children.allObjects as? [DataSnapshot] {
+                    for emailSnapshot in emailsSnapshot {
+                        if let emailDict = emailSnapshot.value as? [String: Any],
+                           let storedEmail = emailDict["email"] as? String {
+                            if storedEmail == email {
+                                completion(false)
+                                return
+                            }
+                        }
                     }
-                } else {
-                    completion(true)
                 }
+                completion(true)
             } else {
                 completion(true)
             }
         }
     }
-
-    func deleteAccountWithEmail(_ email: String, completion: @escaping (Error?) -> Void) {
-        let emailsRef = DB_REF.child("emails")
-        emailsRef.observeSingleEvent(of: .value) { (snapshot) in
-            guard snapshot.exists(), var emails = snapshot.value as? [String: Any] else {
-                completion(nil)
-                return
-            }
-
-            if let storedEmail = emails["email"] as? String, storedEmail == email {
+    func deleteAccountWithEmail(_ uid: String, completion: @escaping (Error?) -> Void) {
+        if let uid = Auth.auth().currentUser?.uid {
+            let emailsRef = DB_REF.child("emails").child(uid)
+            
+            emailsRef.observeSingleEvent(of: .value) { (snapshot) in
+                guard snapshot.exists() else {
+                    completion(nil)
+                    return
+                }
                 emailsRef.removeValue { (error, _) in
                     completion(error)
                 }
-            } else {
-                completion(nil)
             }
+        } else {
+            completion(nil)
         }
     }
-
     
     func logUserIn(withEmail email: String, password: String, completion: @escaping(AuthDataResult?, Error?) -> Void) {
         Auth.auth().signIn(withEmail: email, password: password, completion: completion)
     }
-    
 }
